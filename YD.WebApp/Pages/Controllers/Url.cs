891561b0f1ab14ce3dd22617d7861c191a59094b
Tung Cheng <ga2006088445@gmail.com>
2020-08-16 23:16:58 +0800

    修正存入正確名字

--- YD.WebApp/Pages/Controllers/Url.cs
+++ YD.WebApp/Pages/Controllers/Url.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using YD.Lib;
@@ -23,7 +25,15 @@ namespace Youtube_download.Controllers
                 {
                     if (url != "")
                     {
-                        Data newData = new Data() { url = url, name = url };
+                        //得到歌名
+                        var web = new HtmlWeb();
+                        var doc = web.Load(url);
+                        var resp = doc.Text;
+                        Regex rg = new Regex(@"<title>(.*)</title>");
+                        var name = rg.Match(resp).Groups[1].Value.Replace(" - YouTube", "");
+
+                        //判斷不重復歌單
+                        Data newData = new Data() { url = url, name = name };
                         bool repeat = false;
                         dalist.ForEach(x =>
                         {
