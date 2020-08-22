95c5541d0d44d27ce7ba4231536e4daa3212f8ba
Tung Cheng <ga2006088445@gmail.com>
2020-08-22 12:39:34 +0800

    新增歌單訊息更完善

--- YD.WebApp/Pages/Controllers/Url.cs
+++ YD.WebApp/Pages/Controllers/Url.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -15,46 +16,58 @@ namespace Youtube_download.Controllers
         [HttpPost]
         public object SaveTotxt(List<Data> joinDataList)
         {
-            try
+            var success = true;
+            var msg = "歌單新增成功\n";
+            var oldDataList = LoadSong.LoadList();
+            Parallel.ForEach(joinDataList, async forDataList =>
             {
-                //var newDataList = new List<Data>();
-                var oldDataList = LoadSong.LoadList();
-                Parallel.ForEach(joinDataList, async forDataList =>
+                var url = forDataList.value;
+                if (url != "")
                 {
-                    var url = forDataList.value;
-                    if (url != "")
+                    try
                     {
-                        //得到歌名
-                        var web = new HtmlWeb();
-                        var doc = await web.LoadFromWebAsync(url);
-                        var resp = doc.Text;
-                        Regex rg1 = new Regex(@"<title>(.*)</title>");
-                        var name = rg1.Match(resp).Groups[1].Value.Replace(" - YouTube", "");
+                        //得到v
                         Regex rg2 = new Regex(@"v=([\w\-]*)\b");
+                        if (rg2.IsMatch(url))
+                        {
+                            var val = rg2.Match(url).Groups[1].Value;
+                            //得到歌名
+                            var web = new HtmlWeb();
+                            var doc = await web.LoadFromWebAsync(url);
+                            var resp = doc.Text;
+                            Regex rg1 = new Regex(@"<title>(.*)</title>");
+                            var name = rg1.Match(resp).Groups[1].Value.Replace(" - YouTube", "");
 
-                        var newData = new Data() { value = rg2.Match(url).Groups[1].Value, name = name };
-                        //判斷不重復歌單
-                        bool repeat = false;
-                        oldDataList.ForEach(oldData =>
-                        {
-                            if (JsonConvert.SerializeObject(newData) == JsonConvert.SerializeObject(oldData))
+
+                            var newData = new Data() { value = val, name = name };
+                            //判斷不重復歌單
+                            bool repeat = false;
+                            oldDataList.ForEach(oldData =>
                             {
-                                repeat = true;
+                                if (JsonConvert.SerializeObject(newData) == JsonConvert.SerializeObject(oldData))
+                                {
+                                    repeat = true;
+                                }
+                            });
+                            if (!repeat)
+                            {
+                                oldDataList.Add(newData);
                             }
-                        });
-                        if (!repeat)
-                        {
-                            oldDataList.Add(newData);
+                            WriteSong.WriteList(oldDataList);
                         }
-                        WriteSong.WriteList(oldDataList);
+                        else
+                        {
+                            throw new Exception($"{url} 建立錯誤\n");
+                        }
                     }
-                });
-            }
-            catch
-            {
-                return new { success = false };
-            }
-            return new { success = true };
+                    catch (Exception e)
+                    {
+                        success = false;
+                        msg += e.Message;
+                    }
+                }
+            });
+            return new { success = success, msg = msg };
 
         }
 
